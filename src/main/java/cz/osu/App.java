58d62f98698152f40290fcd9994fb41b3e72868e
David Miko <ph4nt0mgh0sT17@gmail.com>
2020-06-08 12:59:28 +0200

    Added some new features for logging. Just testing the slf4j.

--- src/main/java/cz/osu/App.java
+++ src/main/java/cz/osu/App.java
@@ -3,6 +3,8 @@ package cz.osu;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Scanner;
+
 public class App {
 
 
@@ -13,6 +15,18 @@ public class App {
         logger.info("The program has started.");
 
         System.out.println("Hello, world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Type a first number: ");
+        int firstNumber = scanner.nextInt();
+
+        System.out.print("Type a second number: ");
+        int secondNumber = scanner.nextInt();
+
+        Calculator calc = new Calculator();
+
+        System.out.println(String.format("%d / %d = %d",firstNumber, secondNumber, calc.divide(firstNumber, secondNumber)));
 
         logger.info("The program has ended.");
     }
--- src/main/java/cz/osu/Calculator.java
+++ src/main/java/cz/osu/Calculator.java
@@ -1,8 +1,27 @@
 package cz.osu;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Calculator {
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
 
     public int add(int firstNumber, int secondNumber) {
         return firstNumber + secondNumber;
+    }
+
+    public int divide(int firstNumber, int secondNumber) {
+
+        try {
+            return firstNumber / secondNumber;
+        }
+
+        catch (ArithmeticException ex) {
+            logger.error("Division by zero.", ex);
+            ex.printStackTrace();
+
+            throw ex;
+        }
     }
 }
