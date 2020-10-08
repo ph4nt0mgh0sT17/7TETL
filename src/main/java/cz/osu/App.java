f5a540e591b785c5c93a9950dd194f9214048eee
David Miko <ph4nt0mgh0sT17@gmail.com>
2020-10-08 15:04:40 +0200

    Added exercise.

--- src/main/java/cz/osu/App.java
+++ src/main/java/cz/osu/App.java
@@ -15,24 +15,62 @@ public class App {
 
         logger.info("The program has started.");
 
-        System.out.println("Hello, world!");
+        int a = 10;
+        int b = 0;
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Type a first number: ");
-        int firstNumber = scanner.nextInt();
-
-        System.out.print("Type a second number: ");
-        int secondNumber = scanner.nextInt();
-
-        Calculator calc = new Calculator();
-
-        System.out.println(String.format("%d / %d = %d",firstNumber, secondNumber, calc.divide(firstNumber, secondNumber)));
+        int c = a / b;
 
         logger.info("The program has ended.");
     }
 
-    public static String getName(String name) {
-        return name;
+    public static double calculateArithmeticMean(double[] data) {
+        assert data != null : "The data are expected to not be null.";
+
+        return Double.parseDouble(null);
+    }
+
+    private static int[] createIntersectionBetweenArrays(int[] a, int[] b) {
+
+        assert a != null;
+        assert a.length != 0;
+
+        assert b != null;
+        assert b.length != 0;
+
+        int[] ret = null;
+
+        int nextIndex = 0;
+
+        
+        Integer[] tmp = new Integer[Math.min(a.length, b.length)];
+
+        assert tmp.length <= a.length && tmp.length <= b.length;
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b.length; j++) {
+                if (a[i] == b[j]) {
+                    tmp[nextIndex] = a[i];
+                    nextIndex++;
+                }
+            }
+        }
+        
+        for (int i = 0; i < tmp.length; i++) {
+            if (tmp[i] == null) {
+                ret = new int[i];
+                break;
+            }
+        }
+
+        if (ret == null)
+            ret = new int[tmp.length];
+
+        assert ret != null;
+
+        for (int i = 0; i < ret.length; i++) {
+            ret[i] = tmp[i];
+        }
+
+        return ret;
     }
 }
